1d05496e2678d749e5e8498b9ddf8fef3d927cce
Kivanc Yazan <kivancyazan@gmail.com>
2015-03-28 21:19:08 +0200

    create community

--- create_community.php
+++ create_community.php
@@ -1,5 +1,30 @@
-<?php include 'header.php';?>
+<?php include('header.php');
+if(!$loggedin){
+	$_SESSION['AlertRed'] = "You have to be logged in to do that.";
+	header("location:index.php");
+}?>
 
-x
+<div class="container">
 
-<?php include 'footer.php';?>
+      <form class="form-signin" method="post" action="create_community_check.php">
+        <h2 class="form-signin-heading">Create Community</h2>
+        <label for="inputName" class="sr-only">Community name</label>
+        <input type="name" name="inputName" class="form-control" 
+        maxlength=32 placeholder="Community name" required autofocus>
+        <label for="inputDesc" class="sr-only">Description</label>
+        <input type="name" name="inputDesc" class="form-control" 
+        maxlength=255 placeholder="Description" required>
+        <label for="inputPrivacy" class="sr-only">Privacy</label>
+        <div class="radio">
+ 			<label><input type="radio" name="inputPrivacy" value="public" checked="checked">Public (Anyone can join, anyone can read)</label>
+		</div>
+		<div class="radio">
+  			<label><input type="radio" name="inputPrivacy" value="private">Private (Approve to join, members can read)</label>
+		</div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Create</button>
+      </form>
+
+    </div> <!-- /container -->
+
+
+<?php include('footer.php');?>
--- create_community_check.php
+++ create_community_check.php
@@ -1,5 +1,46 @@
-<?php include 'header.php';?>
+<?php include('header.php');
+if(!$loggedin){
+	$_SESSION['AlertRed'] = "You have to be logged in to do that.";
+	header("location:index.php");
+}else{
 
-x
+	// email, username and password sent from form
+	$CommName=$_POST['inputName'];
+	$CommDesc=$_POST['inputDesc'];
+	$CommPriv=$_POST['inputPrivacy'];
 
-<?php include 'footer.php';?>
+	if(empty($CommName) || empty($CommDesc) || empty($CommPriv)){
+			$_SESSION['AlertRed'] = "Don't leave blank fields.";
+			header("location:create_community.php");
+	}else{
+
+		require_once("config.php"); //Get db credentials
+
+		$stmt=$db->prepare("SELECT * FROM Comms WHERE CommName=?");
+		$stmt->execute(array($CommName));
+		$numrows = $stmt->rowCount();
+		
+		if($numrows1>0){
+			$_SESSION['AlertRed'] = "Community with the same name already exists.";
+			header("location:create_community.php");
+		}else{
+		
+			//Create community
+			$stmt = $db->prepare("INSERT INTO Comms (CommName,ShortDesc,Privacy,CreatedOn) VALUES (?,?,?,NOW());");
+			$stmt->execute(array($CommName,$CommDesc,$CommPriv));
+			$commid = $db->lastInsertId();
+
+			//and add the user who created it as the first member.
+			$stmt = $db->prepare("INSERT INTO UsersInComms (UserID,CommID,JoinedOn,Role) VALUES (?,?,NOW(),'admin');");
+			$stmt->execute(array($userid,$commid));
+
+			$_SESSION['AlertGreen'] = "Successfully created community $CommName!";
+			header("location:index.php");		
+
+		}
+		
+		$db=null;//close connection
+
+	}
+}
+?>
