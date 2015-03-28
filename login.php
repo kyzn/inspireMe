0d9ca0450d5a548b6c98a047fcda366e64b88c66
Kivanc Yazan <kivancyazan@gmail.com>
2015-03-28 18:03:30 +0200

    maxlength added

--- login.php
+++ login.php
@@ -5,9 +5,11 @@
       <form class="form-signin" method="post" action="login_check.php">
         <h2 class="form-signin-heading">Sign in</h2>
         <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" name="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <input type="email" name="inputEmail" class="form-control" 
+        maxlength=255 placeholder="Email address" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" name="inputPassword" class="form-control" placeholder="Password" required>
+        <input type="password" name="inputPassword" class="form-control" 
+        maxlength=16 placeholder="Password" required>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
       </form>
 
--- register.php
+++ register.php
@@ -5,12 +5,15 @@
       <form class="form-signin" method="post" action="register_check.php">
         <h2 class="form-signin-heading">Register</h2>
         <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" name="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="name" name="inputUsername" class="form-control" placeholder="Username" required>
+        <input type="email" name="inputEmail" class="form-control" 
+        maxlength=255 placeholder="Email address" required autofocus>
+        <label for="inputEmail" class="sr-only">Username</label>
+        <input type="name" name="inputUsername" class="form-control" 
+        maxlength=32 placeholder="Username" required>
 
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" name="inputPassword" class="form-control" placeholder="Password" required>
+        <input type="password" name="inputPassword" class="form-control" 
+        maxlength=16 placeholder="Password" required>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
       </form>
 
