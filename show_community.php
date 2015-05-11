81cc4acef07e5dd1f538dc677e1e29ecfd8880b1
Kivanc Yazan <kivancyazan@gmail.com>
2015-05-12 00:27:50 +0300

    improved post list now lists upvotes for each post along with the creation date user can sort the post list according to number of upvotes and creating date, in descending or ascending or by double clicking this is somewhat related to #15

--- show_community.php
+++ show_community.php
@@ -14,11 +14,55 @@ if (!isset($_GET['comm_id'])) {
     $stmt=$db->prepare("SELECT * FROM Comms WHERE CommID=?");
 	$stmt->execute(array($commid));
 	$numrows = $stmt->rowCount();
-	
+
 	if($numrows==0){
 		$_SESSION['AlertRed'] = "No such community can be found.";
 		header("location:index.php");
 	}else{
+
+
+		$upvotessortnext="_desc";
+		$chronosortnext ="_desc";
+		
+		if(isset($_GET['order_by'])){
+			$orderby=$_GET['order_by'];
+
+			if($orderby=="upvotes"){
+				$orderby="upvotes_desc";
+				$upvotessortnext="_asc";
+			}else if($orderby=="upvotes_desc"){
+				$upvotessortnext="_asc";
+			}else if($orderby=="upvotes_asc"){
+				;
+			}else if($orderby=="chrono"){
+				$orderby="chrono_desc";
+				$chronosortnext="_asc";
+			}else if($orderby=="chrono_desc"){
+				$chronosortnext="_asc";
+			}else if($orderby=="chrono_asc"){
+				;
+			}else{
+				$orderby="chrono_desc"; #this is going to be default
+				$chronosortnext ="_asc";
+			}
+
+		}else{
+			$orderby="chrono_desc"; #this is going to be default
+			$chronosortnext ="_asc";
+
+		}
+
+
+		if($orderby=="chrono_desc"){
+			$orderstmt="ORDER BY date desc;";
+		}else if($orderby=="chrono_asc"){
+			$orderstmt="ORDER BY date asc;";
+		}else if($orderby=="upvotes_desc"){
+			$orderstmt="ORDER BY upvotes desc;";
+		}else if($orderby=="upvotes_asc"){
+			$orderstmt="ORDER BY upvotes asc;";
+		}
+
 		//Save the privacy setting of the group aside
 		$row = $stmt->fetch(PDO::FETCH_ASSOC);
 		if($row['Privacy']=='private'){$private=true;}else{$private=false;}
@@ -83,6 +127,8 @@ if (!isset($_GET['comm_id'])) {
       		<table class='table table-striped'>
         	<thead>
           	<tr>
+          	<th width=5><a href=\"./show_community.php?comm_id=".$commid."&order_by=upvotes".$upvotessortnext."\">Upvotes</a></th>
+          	<th><a href=\"./show_community.php?comm_id=".$commid."&order_by=chrono".$chronosortnext."\">Date</a></th>
             <th>Post Title</th>
             <th>Author</th>
           	</tr>
@@ -90,12 +136,18 @@ if (!isset($_GET['comm_id'])) {
         	<tbody>";
 
 
-			$query = "SELECT P.PostID pid, P.PostTitle ptitle, U.UserName uname, U.UserID uid 
-			FROM Posts P, Users U WHERE P.UserID=U.UserID AND P.CommID=".$commid.";";
+			$query = $db->prepare("SELECT P.PostID pid, P.PostTitle ptitle, U.UserName uname, U.UserID uid,
+			(SELECT COUNT(*) FROM UpvotesForPosts UP WHERE UP.PostID=P.PostID AND UP.IsDeleted=0) as upvotes,
+			P.CreatedOn AS date  
+			FROM Posts P, Users U
+			WHERE P.UserID=U.UserID AND P.CommID=? 
+			".$orderstmt);
+			$query->execute(array($commid));
 
-
-			foreach($db->query($query) as $row){
+			while($row = $query->fetch ( PDO::FETCH_ASSOC )){
 				echo"<tr>
+				<td>".$row['upvotes']."</td>
+				<td>".$row['date']."</td>
 				<td><a href='./show_post.php?post_id=".$row['pid']."'>".$row['ptitle']."</a></td>
 				<td><a href='./show_user.php?user_id=".$row['uid']."'>".$row['uname']."</a></td>
 				</tr>";
