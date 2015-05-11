f3cf7edaceb9ff77254a6ff091fb3f27b388e17e
ibrahim cimentepe <ibrahim.cimentepe@gmail.com>
2015-05-12 00:39:15 +0300

    If User-id is not passed (?user_id=1) it should show logged in users profile (it does not)  : Solved

--- show_user.php
+++ show_user.php
@@ -6,58 +6,62 @@ if (! $loggedin) {
    header ( "location:index.php" );
 }
 
-if (! isset ( $_GET ['user_id'] )) {
-   $_SESSION ['AlertRed'] = "No such post can be found.";
+if (! isset ( $_GET ['user_id'] )) 
+{
+   // $_SESSION ['AlertRed'] = "No such post can be found.";
    // header("location:index.php");
-} else {
+}
+else 
+{
    $userid = $_GET ['user_id'];
+}
+
+$stmt = $db->prepare ( "SELECT * FROM users WHERE UserID=?" );
+$stmt->execute ( array (
+      $userid 
+) );
+$numrows = $stmt->rowCount ();
+
+if ($numrows == 0) {
+   $_SESSION ['AlertRed'] = "No such user can be found.";
+   header ( "location:index.php" );
+} else {
+   $row = $stmt->fetch ( PDO::FETCH_ASSOC );
    
-   $stmt = $db->prepare ( "SELECT * FROM users WHERE UserID=?" );
+   $displayedUserID = $row ['UserID'];
+   $userEmail = $row ['Email'];
+   $userName = $row ['UserName'];
+   $regDate = $row ['RegDate'];
+   
+   // total num of posts
+   $stmt = $db->prepare ( "SELECT * FROM posts WHERE UserID=?" );
    $stmt->execute ( array (
-         $userid 
+         $displayedUserID 
    ) );
-   $numrows = $stmt->rowCount ();
+   $totalnumofposts = $stmt->rowCount ();
    
-   if ($numrows == 0) {
-      $_SESSION ['AlertRed'] = "No such user can be found.";
-      header ( "location:index.php" );
-   } else {
-      $row = $stmt->fetch ( PDO::FETCH_ASSOC );
-      
-      $displayedUserID = $row ['UserID'];
-      $userEmail = $row ['Email'];
-      $userName = $row ['UserName'];
-      $regDate = $row ['RegDate'];
-      
-      // total num of posts
-      $stmt = $db->prepare ( "SELECT * FROM posts WHERE UserID=?" );
-      $stmt->execute ( array (
-            $displayedUserID 
-      ) );
-      $totalnumofposts = $stmt->rowCount ();
-      
-      // total num of comments
-      $stmt = $db->prepare ( "SELECT * FROM comments WHERE UserID=?" );
-      $stmt->execute ( array (
-            $displayedUserID 
-      ) );
-      $totalnumofcomments = $stmt->rowCount ();
-      
-      // total num of upvotes
-      $stmt = $db->prepare ( "SELECT * FROM upvotesforposts WHERE UserID=?" );
-      $stmt->execute ( array (
-            $displayedUserID 
-      ) );
-      $totalnumofupvotes = $stmt->rowCount ();
-      
-      // total num of likes
-      $stmt = $db->prepare ( "SELECT * FROM upvotesforcomments WHERE UserID=?" );
-      $stmt->execute ( array (
-            $displayedUserID 
-      ) );
-      $totalnumoflikes = $stmt->rowCount ();
-      
-      ?>
+   // total num of comments
+   $stmt = $db->prepare ( "SELECT * FROM comments WHERE UserID=?" );
+   $stmt->execute ( array (
+         $displayedUserID 
+   ) );
+   $totalnumofcomments = $stmt->rowCount ();
+   
+   // total num of upvotes
+   $stmt = $db->prepare ( "SELECT * FROM upvotesforposts WHERE UserID=?" );
+   $stmt->execute ( array (
+         $displayedUserID 
+   ) );
+   $totalnumofupvotes = $stmt->rowCount ();
+   
+   // total num of likes
+   $stmt = $db->prepare ( "SELECT * FROM upvotesforcomments WHERE UserID=?" );
+   $stmt->execute ( array (
+         $displayedUserID 
+   ) );
+   $totalnumoflikes = $stmt->rowCount ();
+   
+   ?>
 
 <hr>
 <div class="container">
@@ -116,18 +120,18 @@ if (! isset ( $_GET ['user_id'] )) {
 				<div class="panel-heading">Communities</div>
 				<div class="panel-body">
 				  <?php
-      $query = "SELECT C.CommID cid, C.CommName cname
+   $query = "SELECT C.CommID cid, C.CommName cname
       FROM Comms C, Usersincomms U WHERE C.CommID=U.CommID AND U.UserID=" . $userid . ";";
-      
-      foreach ( $db->query ( $query ) as $row ) {
-         echo "<p>
+   
+   foreach ( $db->query ( $query ) as $row ) {
+      echo "<p>
 					<h4>
                         <a href='./show_community.php?comm_id=" . $row ['cid'] . "'>" . $row ['cname'] . "</a></td>
 				    </h4>
 			   </p>";
-      }
-      
-      ?>
+   }
+   
+   ?>
 				
 				
 <!-- 					<p>
@@ -163,8 +167,8 @@ if (! isset ( $_GET ['user_id'] )) {
 				<div class="tab-pane active" id="home">
 					<div class="table-responsive">
 						<?php
-      
-      echo "
+   
+   echo "
       		<table class='table table-striped'>
         	<thead>
           	<tr>
@@ -173,17 +177,17 @@ if (! isset ( $_GET ['user_id'] )) {
           	</tr>
         	</thead>
         	<tbody>";
-      
-      $query = "SELECT P.PostID pid, P.PostTitle ptitle, P.CreatedOn pdate
+   
+   $query = "SELECT P.PostID pid, P.PostTitle ptitle, P.CreatedOn pdate
 			FROM Posts P WHERE P.UserID=" . $userid . ";";
-      
-      foreach ( $db->query ( $query ) as $row ) {
-         echo "<tr>
+   
+   foreach ( $db->query ( $query ) as $row ) {
+      echo "<tr>
 				<td><a href='./show_post.php?post_id=" . $row ['pid'] . "'>" . $row ['ptitle'] . "</a></td>
                <td>" . $row ['pdate'] . "</td>
 				</tr>";
-      }
-      ?>
+   }
+   ?>
 						</tbody>
 						</table>
 					</div>
@@ -203,18 +207,18 @@ if (! isset ( $_GET ['user_id'] )) {
 					<table class="table table-hover">
 						<tbody>
                            <?php
-      $query = "SELECT P.PostID pid, P.PostTitle ptitle, C.CreatedOn cdate 
+   $query = "SELECT P.PostID pid, P.PostTitle ptitle, C.CreatedOn cdate 
                                         FROM Posts P, Comments C 
                                         WHERE C.UserID=" . $userid . " AND P.PostID=C.PostID 
                                         ORDER BY C.CreatedOn DESC LIMIT 10;";
-      
-      foreach ( $db->query ( $query ) as $row ) {
-         echo "<tr><td><i class='pull-right fa fa-edit'></i>";
-         echo $row ['cdate'] . " - Commented on the post 
+   
+   foreach ( $db->query ( $query ) as $row ) {
+      echo "<tr><td><i class='pull-right fa fa-edit'></i>";
+      echo $row ['cdate'] . " - Commented on the post 
                                       <a href='./show_post.php?post_id=" . $row ['pid'] . "#comments'>" . $row ['ptitle'] . "</a>";
-         echo "</td></tr>";
-      }
-      ?>
+      echo "</td></tr>";
+   }
+   ?>
 						</tbody>
 					</table>
 				</div>
@@ -225,20 +229,20 @@ if (! isset ( $_GET ['user_id'] )) {
 					<table class="table table-hover">
 						<tbody>
                            <?php
-      $query = "SELECT P.PostID pid, P.PostTitle ptitle, C.CreatedOn cdate, U.UserID uid, U.UserName uname
+   $query = "SELECT P.PostID pid, P.PostTitle ptitle, C.CreatedOn cdate, U.UserID uid, U.UserName uname
 			                                   FROM Posts P, Users U, Comments C 
                                                WHERE P.UserID=" . $userid . " AND P.PostID=C.PostID AND U.UserID=C.UserID
                                                ORDER BY C.CreatedOn DESC LIMIT 10;";
-      
-      foreach ( $db->query ( $query ) as $row ) {
-         if ($row ['uid'] != $userid) {
-            echo "<tr><td><i class='pull-right fa fa-edit'></i>";
-            echo $row ['cdate'] . " - " . $row ['uname'] . " commented on your post
+   
+   foreach ( $db->query ( $query ) as $row ) {
+      if ($row ['uid'] != $userid) {
+         echo "<tr><td><i class='pull-right fa fa-edit'></i>";
+         echo $row ['cdate'] . " - " . $row ['uname'] . " commented on your post
                                       <a href='./show_post.php?post_id=" . $row ['pid'] . "#comments'>" . $row ['ptitle'] . "</a>";
-            echo "</td></tr>";
-         }
+         echo "</td></tr>";
       }
-      ?>
+   }
+   ?>
 						</tbody>
 					</table>
 				</div>
@@ -249,18 +253,18 @@ if (! isset ( $_GET ['user_id'] )) {
 					<table class="table table-hover">
 						<tbody>
                            <?php
-      $query = "SELECT P.PostID pid, P.PostTitle ptitle, UP.CreatedOn udate
+   $query = "SELECT P.PostID pid, P.PostTitle ptitle, UP.CreatedOn udate
 			                                   FROM Posts P, Upvotesforposts UP
                                                WHERE UP.UserID=" . $userid . " AND P.PostID=UP.PostID AND UP.IsDeleted=0
                                                ORDER BY UP.CreatedOn DESC LIMIT 10;";
-      
-      foreach ( $db->query ( $query ) as $row ) {
-         echo "<tr><td><i class='pull-right fa fa-edit'></i>";
-         echo $row ['udate'] . " - Upvoted the post
+   
+   foreach ( $db->query ( $query ) as $row ) {
+      echo "<tr><td><i class='pull-right fa fa-edit'></i>";
+      echo $row ['udate'] . " - Upvoted the post
               <a href='./show_post.php?post_id=" . $row ['pid'] . "'>" . $row ['ptitle'] . "</a>";
-         echo "</td></tr>";
-      }
-      ?>
+      echo "</td></tr>";
+   }
+   ?>
 						</tbody>
 					</table>
 				</div>
@@ -271,18 +275,18 @@ if (! isset ( $_GET ['user_id'] )) {
 					<table class="table table-hover">
 						<tbody>
                            <?php
-      $query = "SELECT P.PostID pid, P.PostTitle ptitle, UP.CreatedOn cdate, U.UserID uid, U.UserName uname
+   $query = "SELECT P.PostID pid, P.PostTitle ptitle, UP.CreatedOn cdate, U.UserID uid, U.UserName uname
 			                                    FROM Posts P, Users U, Upvotesforposts UP
                                                 WHERE P.UserID=" . $userid . " AND P.PostID=UP.PostID AND U.UserID=UP.UserID
                                                 ORDER BY UP.CreatedOn DESC LIMIT 10;";
-      
-      foreach ( $db->query ( $query ) as $row ) {
-         echo "<tr><td><i class='pull-right fa fa-edit'></i>";
-         echo $row ['cdate'] . " - " . $row ['uname'] . " upvoted your post
+   
+   foreach ( $db->query ( $query ) as $row ) {
+      echo "<tr><td><i class='pull-right fa fa-edit'></i>";
+      echo $row ['cdate'] . " - " . $row ['uname'] . " upvoted your post
               <a href='./show_post.php?post_id=" . $row ['pid'] . "'>" . $row ['ptitle'] . "</a>";
-         echo "</td></tr>";
-      }
-      ?>
+      echo "</td></tr>";
+   }
+   ?>
 						</tbody>
 					</table>
 				</div>
@@ -293,18 +297,18 @@ if (! isset ( $_GET ['user_id'] )) {
 					<table class="table table-hover">
 						<tbody>
                            <?php
-      $query = "SELECT P.PostID pid, P.PostTitle ptitle, UC.CreatedOn udate
+   $query = "SELECT P.PostID pid, P.PostTitle ptitle, UC.CreatedOn udate
 			                                   FROM Posts P, Upvotesforcomments UC, Comments C
                                                WHERE UC.UserID=" . $userid . " AND P.PostID=C.PostID AND UC.CommentID=C.CommentID AND UC.IsDeleted=0
                                                ORDER BY UC.CreatedOn DESC LIMIT 10;";
-      
-      foreach ( $db->query ( $query ) as $row ) {
-         echo "<tr><td><i class='pull-right fa fa-edit'></i>";
-         echo $row ['udate'] . " - Liked the comment in the post 
+   
+   foreach ( $db->query ( $query ) as $row ) {
+      echo "<tr><td><i class='pull-right fa fa-edit'></i>";
+      echo $row ['udate'] . " - Liked the comment in the post 
               <a href='./show_post.php?post_id=" . $row ['pid'] . "#comments'>" . $row ['ptitle'] . "</a>";
-         echo "</td></tr>";
-      }
-      ?>
+      echo "</td></tr>";
+   }
+   ?>
 						</tbody>
 					</table>
 				</div>
@@ -315,18 +319,18 @@ if (! isset ( $_GET ['user_id'] )) {
 					<table class="table table-hover">
 						<tbody>
                            <?php
-      $query = "SELECT P.PostID pid, P.PostTitle ptitle, UC.CreatedOn cdate, U.UserID uid, U.UserName uname
+   $query = "SELECT P.PostID pid, P.PostTitle ptitle, UC.CreatedOn cdate, U.UserID uid, U.UserName uname
 			                                    FROM Posts P, Users U, Upvotesforcomments UC, Comments C
                                                 WHERE C.UserID=" . $userid . " AND UC.CommentID=C.CommentID AND U.UserID=UC.UserID AND P.PostID=C.PostID
 				                                ORDER BY UC.CreatedOn DESC";
-      
-      foreach ( $db->query ( $query ) as $row ) {
-         echo "<tr><td><i class='pull-right fa fa-edit'></i>";
-         echo $row ['cdate'] . " - ".$row['uname']." liked your comment in the post 
+   
+   foreach ( $db->query ( $query ) as $row ) {
+      echo "<tr><td><i class='pull-right fa fa-edit'></i>";
+      echo $row ['cdate'] . " - " . $row ['uname'] . " liked your comment in the post 
               <a href='./show_post.php?post_id=" . $row ['pid'] . "#comments'>" . $row ['ptitle'] . "</a>";
-         echo "</td></tr>";
-      }
-      ?>
+      echo "</td></tr>";
+   }
+   ?>
 						</tbody>
 					</table>
 				</div>
@@ -341,7 +345,6 @@ if (! isset ( $_GET ['user_id'] )) {
 <!--/row-->
 
 <?php
-   }
 }
 ?>
 
